01f9cc2ea133ce8c21e6ebe7dd09576cb99d683c
Sue Youki <sue39pyw@gmail.com>
2020-09-06 23:24:14 +0900

    [add]vue-scrollto, works component, deleted Interior component

---
+++ frontend/yotch/pages/works.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <Heading2>Works</Heading2>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  margin-top: 80px;
+  max-width: 1030px;
+  min-height: 100vh;
+  width: 90%;
+}
+
+@media screen and (max-width: 767px) {
+  .container {
+    width: 100%;
+  }
+}
+</style>
